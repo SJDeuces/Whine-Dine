a52fb3eb27ad5b6c72ffc082241b3c8bba55efe8
Peter Nouv <PeterNouv@gmail.com>
2016-01-30 05:18:06 -0800

    new activity

--- app/src/main/java/com/example/sjdeuces/hackucsc/LauncherScreen.java
+++ app/src/main/java/com/example/sjdeuces/hackucsc/LauncherScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class LauncherScreen extends AppCompatActivity {
 
@@ -12,8 +13,13 @@ public class LauncherScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher_screen);
     }
-    public void goToMenu(View v){
+    public void goToCollege(View v){
         Intent intent = new Intent(this, CollegeActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToSettings(View v){
+        Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 }
