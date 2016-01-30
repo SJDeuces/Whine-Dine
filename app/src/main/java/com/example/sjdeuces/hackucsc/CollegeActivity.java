5ee4be879844f9a17c84a03363f1d80be7a60beb
Peter Nouv <PeterNouv@gmail.com>
2016-01-30 01:17:19 -0800

    Merge branch 'master' of C:\Users\Peter\Documents\GitHub\Hackucsc with conflicts.

--- app/src/main/java/com/example/sjdeuces/hackucsc/CollegeActivity.java
+++ app/src/main/java/com/example/sjdeuces/hackucsc/CollegeActivity.java
@@ -3,12 +3,11 @@ package com.example.sjdeuces.hackucsc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CollegeActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 }
--- app/src/main/java/com/example/sjdeuces/hackucsc/LauncherScreen.java
+++ app/src/main/java/com/example/sjdeuces/hackucsc/LauncherScreen.java
@@ -1,7 +1,9 @@
 package com.example.sjdeuces.hackucsc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LauncherScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LauncherScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher_screen);
+    }
+
+    public void goToMenu(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
