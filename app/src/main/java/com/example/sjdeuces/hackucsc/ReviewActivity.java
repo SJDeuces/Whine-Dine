954696c056be6f9442fd42ab3ac4144511a8dbb6
SJDeuces <Stanleyjohnsonii@gmail.com>
2016-01-30 14:03:01 -0800

    Launcher screen

--- app/src/main/java/com/example/sjdeuces/hackucsc/ReviewActivity.java
+++ app/src/main/java/com/example/sjdeuces/hackucsc/ReviewActivity.java
@@ -9,5 +9,6 @@ public class ReviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+        //test
     }
 }
